b64ea55c33d5d230042f2ff2ecf4cb1a7d49d8c0
Celita Cintas <cintas.celia@gmail.com>
2014-09-16 10:39:04 -0300

    svm and erc

--- methods.py
+++ methods.py
@@ -1,32 +1,81 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
+import os
 import argparse
 import numpy as np
-import pandas as pd
-from pre_process import get_pheno, load_npz, merge_geno_pheno, \
+from sklearn.decomposition import RandomizedPCA
+from sklearn.svm import SVC
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.cross_validation import train_test_split
+from pre_process import load_npz, merge_geno_pheno, RawDataPheno, \
 						normalization
-import os
+from plot_tools import plot_PCA, plot_confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report
+from matplotlib import pyplot as plt
 
-
-
-
-def main(filenames):
+def get_PCA(X):
 	"""
 	"""
-	ids, X, snps = [], [], []
+	# Project the data to a 2D space for visualization
+	Xp = RandomizedPCA(n_components=2, random_state=1).fit_transform(X)
+	return Xp
+
+def extra_tree(X_train, y_train):
+	"""
+	"""
+	clf = ExtraTreesClassifier(n_estimators=4,
+							   criterion='gini',
+	                           max_features=0.2, 
+	                           n_jobs=4,
+	                           random_state=1).fit(X_train, y_train)
+	#y_pred = clf.predict(X_test)
+
+	return clf
+
+def support_vector(X_train, y_train):
+	"""
+	"""
+	clf = SVC(kernel='linear')
+	clf.fit(X_train, y_train)
+	
+	return clf
+
+def main(filenames, filename_pheno, phenos):
+	"""
+	"""
+	ids, X = [], []
+	snps = None
 	for file in filenames:
-		ids_t, X_t, snps_t = load_npz(file)
+		ids_t, X_t, snps = load_npz(file)
 		ids.append(ids_t)
 		X.append(X_t)
-		snps.append(snps_t)
+	# This should go away
+
 	ids = np.concatenate(ids, axis=0)
 	X = np.concatenate(X, axis=0)
-	merged_df = merge_geno_pheno(ids, X, snps[0])
-	print merged_df
-	norm_x = normalization(merged_df.ix[:,1:-1])
-	print merged_df.columns.values[1:-1]
 
+	data_pheno = RawDataPheno(filename_pheno, phenos)
+	df_pheno = data_pheno.get_pheno()
+	merged_df = merge_geno_pheno(df_pheno, ids, X, snps)
+
+	norm_x = normalization(merged_df.ix[:,1:-1])
 	
+	X_pca = get_PCA(norm_x)
+	y = merged_df[data_pheno.pheno_name[1]].values
+	plot_PCA(X_pca, y)
+	
+	X_train, X_test, y_train, y_test = train_test_split(norm_x, y)
+	
+	#clf = support_vector(X_train, y_train)
+	clf = extra_tree(X_train, y_train)
+	
+	y_pred = clf.predict(X_test)
+	clf.score(X_test, y_test)
+	print classification_report(y_test, y_pred)
+	print data_pheno.pheno_name[1]
+	plot_confusion_matrix(y_test, y_pred)
+
 
 if __name__ == '__main__':
 
@@ -45,5 +94,14 @@ if __name__ == '__main__':
     else:
         parser.print_help()
         sys.exit(1)
+    
+    #TODO pass by parameter
 
-    main(filenames)
+    filename_pheno = 'data/prediction/EarPhenoNew2NA.txt'
+    #phenos = ['EarProtrusion', 'LobeAttachment', 'LobeSize', 'HelixRolling',
+    #		  'Crushelixexpression', 'SuperiorCrusofantihelixexpression',
+    #		  'Foldofantihelix', 'Darwinstubercle', 'Tragussize', 'Antitragussize']
+    phenos = ['LobeAttachment', 'Crushelixexpression', 'Tragussize']
+    for pheno in phenos:
+    	phenos = ['IID', pheno]
+    	main(filenames, filename_pheno, phenos)
